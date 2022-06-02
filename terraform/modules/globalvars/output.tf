0b0ea2268a4eef8f215ac3af00cbf4f2982cac1f
Bhavin <bhavin.kpatel9@gmail.com>
2022-06-02 05:48:30 +0000

    Adding Dockerfile, ec2 and ecr

---
+++ terraform/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Bhavin"
+    "App"   = "Web"
+    "Program" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "assignment1"
+}
